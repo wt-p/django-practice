ee000c6efc4d0c72040d8776dfe77715cfd03bb7
wt-p <wtbtty9955@gmail.com>
2025-06-22 20:17:34 +0900

    new project

---
+++ boardproject/boardapp/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import authenticate, login
+from .models import BoardModel
+
+# Create your views here.
+
+def signupfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = User.objects.create_user(username, '', password)
+            return render(request, 'signup.html', {'some':100})
+        except IntegrityError:
+            return render(request, 'signup.html', {'error':'このユーザーはすでに登録されています。'})
+
+    return render(request, 'signup.html', {'some':100})
+    # return redirect('login')
+
+def loginfunc(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'login.html', {'context':'logged in'})
+        else:
+            return render(request, 'login.html', {'context':'not logged in'})
+    return render(request, 'login.html', {'context':'get method'})
+
+def listfunc(request):
+    object_list = BoardModel.objects.all()
+    return render(request, 'list.html', {'object_list':object_list})
